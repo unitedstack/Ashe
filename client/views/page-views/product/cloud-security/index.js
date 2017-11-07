10d53b4d84707f22d70859cd9563f0046eb5fee8
cuizhirong <cuizhirong@unitedstack.com>
2017-11-07 11:59:12 +0800

    bugfix

--- client/views/page-views/product/cloud-security/index.js
+++ client/views/page-views/product/cloud-security/index.js
@@ -1,36 +1,19 @@
 require('./style/index.less');
 
-window.onload = window.onresize = window.onscroll = function(){
+window.onload = function(){
   if (!G.mobile) {
     $('.function-item').click(function() {
-      $(this).css({'background':'linear-gradient(54deg, #5A64FA 0%, #28C7EA 100%)','border-radius': '4px 0 0 4px'});
-      $(this).children('.line').css('background', '#ffffff');
-      $(this).children('.function-brief').css('color', '#ffffff');
-      $(this).parents('.center-function').css({'width': '1170px'});
-      $(this).next('.function-intro').css('display', 'block').siblings('.function-intro').css('display', 'none');
-      if($(this).next('.function-intro').css('display', 'block')) {
-        $(this).children('.normal').css('display', 'none');
-        $(this).children('.hover').css('display', 'block');
+      if ($(this).hasClass('hover')) {
+        $(this).removeClass('hover');
+        $(this).parents('.center-function').css({'width': '845px'});
+        $(this).next('.function-intro').css('display', 'none');
+      } else {
+        $(this).addClass('hover');
+        $(this).parents('.center-function').css({'width': '1170px'});
+        $(this).next('.function-intro').css('display', 'block').siblings('.function-intro').css('display', 'none');
       }
-      $(this).siblings('.function-item').css('background', '#ffffff');
-      $(this).siblings('.function-item').children('.hover').css('display', 'none');
-      $(this).siblings('.function-item').children('.normal').css('display', 'block');
-      $(this).siblings('.function-item').children('.line').css('background', 'linear-gradient(90deg, #2FBAEC 0%, #4A83F5 100%)');
-      $(this).siblings('.function-item').children('.function-brief').css('color', '#333333');
-      $(this).siblings('.function-item').mouseenter(function() {
-        $(this).css({'background':'linear-gradient(54deg, #5A64FA 0%, #28C7EA 100%)','border-radius': '4px 0 0 4px'});
-        $(this).children('.line').css('background', '#ffffff');
-        $(this).children('.function-brief').css('color', '#ffffff');
-        $(this).children('.normal').css('display', 'none');
-        $(this).children('.hover').css('display', 'block');
-      });
-      $(this).siblings('.function-item').mouseleave(function() {
-        $(this).siblings('.function-item').css('background', '#ffffff');
-        $(this).siblings('.function-item').children('.hover').css('display', 'none');
-        $(this).siblings('.function-item').children('.normal').css('display', 'block');
-        $(this).siblings('.function-item').children('.line').css('background', 'linear-gradient(90deg, #2FBAEC 0%, #4A83F5 100%)');
-        $(this).siblings('.function-item').children('.function-brief').css('color', '#333333');
-      });
+
+      $(this).siblings('.function-item').removeClass('hover');
     });
   }
   if (G.pad) {
