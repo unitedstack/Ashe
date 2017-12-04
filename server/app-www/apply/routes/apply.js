14f0fb070d7467e3f17145c88c4b6fea7492e6ed
hbshun <bb@bbyy.me>
2017-12-04 15:57:42 +0800

    private cloud

--- server/app-www/apply/routes/apply.js
+++ server/app-www/apply/routes/apply.js
@@ -105,4 +105,33 @@ module.exports = async (router, globalLang, viewsPath) => {
       );
     }
   });
+
+  router.post('/apply/api/private-cloud', checkCaptcha, async (ctx, next) => {
+    ctx.checkBody('nickname').notEmpty('用户名不能为空');
+    ctx.checkBody('phone').notEmpty('电话不能为空');
+    ctx.checkBody('email').notEmpty('邮箱不能为空');
+    ctx.checkBody('company').notEmpty('公司不能为空');
+    if (ctx.errors) {
+      ctx.status = 400;
+      ctx.body = {
+        statusCode: 400,
+        errors: ctx.errors.map(err => {
+          let r = {};
+          tools._.forEach(err, (value, key) => {
+            r.message = value;
+            r.location = key;
+          });
+          return r;
+        })
+      };
+    } else {
+      let body = ctx.request.body;
+      body.evaluation = JSON.stringify(body.evaluation);
+      await controller.applyCloud(body);
+      ctx.body = {
+        statusCode: 200,
+        message: '托管云申请成功'
+      };
+    }
+  });
 };
