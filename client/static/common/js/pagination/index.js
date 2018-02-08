39f6adfe473a055f520ba700fb1c90aca1eaa995
zkn111 <zhangkaina@unitedstack.com>
2018-02-08 13:24:48 +0800

    notice page

--- client/static/common/js/pagination/index.js
+++ client/static/common/js/pagination/index.js
@@ -1,4 +1,4 @@
-function PageNav(params) {
+function PageNav(params){
   this.$box = $('#' + params.boxid);
   this.pageAmount = Number($('.content-main').attr('count'));
   this.page = Number($('.content-main').attr('page'));
@@ -19,20 +19,20 @@ PageNav.prototype.gotoPage = function(number){
   var arrowRight = '<i class="icon-arrow-right"></i>';
   if(Number(number) >= 1 && Number(number) <= this.pageAmount){
     this.page = number;
-  } else {
+  }else{
     this.pageAmount = 1;
     this.page = 1;
   }
   this.$box.empty();
   $('.content-main').attr('page', this.page);
-  if(this.pageAmount <= 7) {
+  if(this.pageAmount <= 7){
     this.page != 1 && $('<a class="cPrev"></a>').html(arrowLeft).appendTo(this.$box);
-    for(var i1 = 0 ; i1 < this.pageAmount ; i1++) {
-      $('<a></a>').addClass('nBtn').html(i1 + 1).appendTo(this.$box);
+    for(var i = 0 ; i < this.pageAmount ; i++){
+      $('<a></a>').addClass('nBtn').html(i + 1).appendTo(this.$box);
     }
     this.page != this.pageAmount && $('<a class="cNext"></a>').html(arrowRight).appendTo(this.$box);
     this.$box.find('.nBtn').eq(this.page - 1).addClass('cur');
-  }else if(this.page < 4) {
+  }else if(this.page < 4){
     this.page != 1 && $('<a></a>').addClass('cPrev').html(arrowLeft).appendTo(this.$box);
     for(var i2 = 1; i2 <= 4; i2 ++) {
       $('<a></a>').addClass('nBtn').html(i2).appendTo(this.$box);
@@ -41,12 +41,12 @@ PageNav.prototype.gotoPage = function(number){
     $('<a></a>').addClass('nBtn').html(this.pageAmount).appendTo(this.$box);
     $('<a></a>').addClass('cNext').html(arrowRight).appendTo(this.$box);
     this.$box.find('.nBtn').eq(this.page - 1).addClass('cur');
-  }else if(this.page <= this.pageAmount - 3) {
+  }else if(this.page <= this.pageAmount - 3){
     $('<a></a>').addClass('cPrev').html(arrowLeft).appendTo(this.$box);
     $('<a></a>').addClass('nBtn').html(1).appendTo(this.$box);
     $('<a></a>').addClass('ellipsis').html('...').appendTo(this.$box);
     for(var i3 = -1; i3 <= 1; i3 ++) {
-      $('<a></a>').addClass(i3 !== 0 ? 'nBtn' : 'nBtn cur').html(this.page + i3).appendTo(this.$box);
+      $('<a></a>').addClass(i !== 0 ? 'nBtn' : 'nBtn cur').html(this.page + i3).appendTo(this.$box);
     }
     $('<a></a>').addClass('ellipsis').html('...').appendTo(this.$box);
     $('<a></a>').addClass('nBtn').html(this.pageAmount).appendTo(this.$box);
@@ -64,22 +64,22 @@ PageNav.prototype.gotoPage = function(number){
   }
   this.bindEvent();
 };
-PageNav.prototype.bindEvent = function() {
+PageNav.prototype.bindEvent = function(){
   var self = this;
-  $('.nBtn').click(function() {
+  $('.nBtn').click(function(){
     var i = parseInt($(this).html());
     self.changeUrl(i);
     self.gotoPage(i);
   });
 
-  $('.cPrev').click(function() {
+  $('.cPrev').click(function(){
     var i = self.page - 1;
     self.changeUrl(i);
     self.gotoPage(i);
     return false;
   });
 
-  $('.cNext').click(function() {
+  $('.cNext').click(function(){
     var i = self.page + 1;
     self.changeUrl(i);
     self.gotoPage(i);
@@ -87,3 +87,6 @@ PageNav.prototype.bindEvent = function() {
   });
 };
 
+new PageNav({
+  'boxid' : 'pn'
+});
